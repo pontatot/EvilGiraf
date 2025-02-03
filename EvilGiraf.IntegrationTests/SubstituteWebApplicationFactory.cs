0111eb273f526682e80c0bc1c80000edda46c584
hugo.du-peloux <hugo.du-peloux@etu.umontpellier.fr>
2025-02-03 11:10:26 +0100

    refactor(test): introduce service substitution in tests
    Replaced `WebApplicationFactory` with `SubstituteWebApplicationFactory` to enable service substitution using NSubstitute. Updated tests to use the substituted `IKubernetes` service, allowing better control and isolation of Kubernetes-related dependencies. Simplified client creation in tests by utilizing the new factory.

---
+++ EvilGiraf.IntegrationTests/SubstituteWebApplicationFactory.cs
@@ -0,0 +1,20 @@
+using k8s;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NSubstitute;
+
+namespace EvilGiraf.IntegrationTests;
+
+public class SubstituteWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = new ServiceDescriptor(typeof(IKubernetes), Substitute.For<IKubernetes>());
+            services.Replace(descriptor);
+        });
+    }
+}
