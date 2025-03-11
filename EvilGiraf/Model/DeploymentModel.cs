9a37c9173725455609f3b970e85133edc5427bd0
hugo.du-peloux <hugo.du-peloux@etu.umontpellier.fr>
2025-03-11 20:36:25 +0100

    refactor: simplify DeploymentService with model extension
    Replaced inline deployment creation logic with a reusable `ToDeployment` extension method in the `DeploymentModel`. This reduces duplication and simplifies the `DeploymentService` logic, improving code maintainability.

--- EvilGiraf/Model/DeploymentModel.cs
+++ EvilGiraf/Model/DeploymentModel.cs
@@ -1,3 +1,52 @@
+using k8s.Models;
+
 namespace EvilGiraf.Model;
 
 public record DeploymentModel(string Name, string Namespace, int Replicas, string Image, int[] Ports);
+
+public static class DeploymentModelExtensions
+{
+    public static V1Deployment ToDeployment(this DeploymentModel model)
+    {
+        return new V1Deployment
+        {
+            Metadata = new V1ObjectMeta
+            {
+                Name = model.Name
+            },
+            Spec = new V1DeploymentSpec
+            {
+                Replicas = model.Replicas,
+                Selector = new V1LabelSelector
+                {
+                    MatchLabels = new Dictionary<string, string>
+                    {
+                        { "app", model.Name }
+                    }
+                },
+                Template = new V1PodTemplateSpec
+                {
+                    Metadata = new V1ObjectMeta
+                    {
+                        Labels = new Dictionary<string, string>
+                        {
+                            { "app", model.Name }
+                        }
+                    },
+                    Spec = new V1PodSpec
+                    {
+                        Containers = new List<V1Container>
+                        {
+                            new()
+                            {
+                                Name = model.Name,
+                                Image = model.Image,
+                                Ports = model.Ports.Select(x => new V1ContainerPort(x)).ToList()
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
--- EvilGiraf/Service/DeploymentService.cs
+++ EvilGiraf/Service/DeploymentService.cs
@@ -6,65 +6,18 @@ using k8s.Models;
 
 namespace EvilGiraf.Service;
 
-public class DeploymentService : IDeploymentService
+public class DeploymentService(IKubernetes client) : IDeploymentService
 {
-    private readonly IKubernetes _client;
-    
-    public DeploymentService(IKubernetes client)
-    {
-        _client = client;
-    }
-    
     public async Task<V1Deployment> CreateDeployment(DeploymentModel model)
     {
-        var deployment = new V1Deployment
-        {
-            Metadata = new V1ObjectMeta
-            {
-                Name = model.Name
-            },
-            Spec = new V1DeploymentSpec
-            {
-                Replicas = model.Replicas,
-                Selector = new V1LabelSelector
-                {
-                    MatchLabels = new Dictionary<string, string>
-                    {
-                        { "app", model.Name }
-                    }
-                },
-                Template = new V1PodTemplateSpec
-                {
-                    Metadata = new V1ObjectMeta
-                    {
-                        Labels = new Dictionary<string, string>
-                        {
-                            { "app", model.Name }
-                        }
-                    },
-                    Spec = new V1PodSpec
-                    {
-                        Containers = new List<V1Container>
-                        {
-                            new()
-                            {
-                                Name = model.Name,
-                                Image = model.Image,
-                                Ports = model.Ports.Select(x => new V1ContainerPort(x)).ToList()
-                            }
-                        }
-                    }
-                }
-            }
-        };
-        return await _client.AppsV1.CreateNamespacedDeploymentAsync(deployment, model.Namespace);
+        return await client.AppsV1.CreateNamespacedDeploymentAsync(model.ToDeployment(), model.Namespace);
     }
     
     public async Task<V1Deployment?> ReadDeployment(string name, string @namespace)
     {
         try
         {
-            var deployment = await _client.AppsV1.ReadNamespacedDeploymentAsync(name, @namespace);
+            var deployment = await client.AppsV1.ReadNamespacedDeploymentAsync(name, @namespace);
             return deployment;
         }
         catch (HttpOperationException)
@@ -77,7 +30,7 @@ public class DeploymentService : IDeploymentService
     {
         try
         {
-            return await _client.AppsV1.DeleteNamespacedDeploymentAsync(name, @namespace);
+            return await client.AppsV1.DeleteNamespacedDeploymentAsync(name, @namespace);
         }
         catch (HttpOperationException)
         {
@@ -87,49 +40,9 @@ public class DeploymentService : IDeploymentService
 
     public async Task<V1Deployment?> UpdateDeployment(DeploymentModel model)
     {
-        var deployment = new V1Deployment
-        {
-            Metadata = new V1ObjectMeta
-            {
-                Name = model.Name
-            },
-            Spec = new V1DeploymentSpec
-            {
-                Replicas = model.Replicas,
-                Selector = new V1LabelSelector
-                {
-                    MatchLabels = new Dictionary<string, string>
-                    {
-                        { "app", model.Name }
-                    }
-                },
-                Template = new V1PodTemplateSpec
-                {
-                    Metadata = new V1ObjectMeta
-                    {
-                        Labels = new Dictionary<string, string>
-                        {
-                            { "app", model.Name }
-                        }
-                    },
-                    Spec = new V1PodSpec
-                    {
-                        Containers = new List<V1Container>
-                        {
-                            new()
-                            {
-                                Name = model.Name,
-                                Image = model.Image,
-                                Ports = model.Ports.Select(x => new V1ContainerPort(x)).ToList()
-                            }
-                        }
-                    }
-                }
-            }
-        };
         try
         {
-            return await _client.AppsV1.ReplaceNamespacedDeploymentAsync(deployment, model.Name, model.Namespace);
+            return await client.AppsV1.ReplaceNamespacedDeploymentAsync(model.ToDeployment(), model.Name, model.Namespace);
         }
         catch (HttpOperationException)
         {
